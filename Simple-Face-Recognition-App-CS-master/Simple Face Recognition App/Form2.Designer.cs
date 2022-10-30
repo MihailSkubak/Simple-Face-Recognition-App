bd9bc89360d879a35926f2817a08faa7a098ae86
Mihail <MihailSkubak>
2022-10-30 13:32:36 +0100

    first commit

---
+++ Simple-Face-Recognition-App-CS-master/Simple Face Recognition App/Form2.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Simple_Face_Recognition_App
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextFromSpeech = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TextFromSpeech
+            // 
+            this.TextFromSpeech.AutoSize = true;
+            this.TextFromSpeech.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextFromSpeech.Location = new System.Drawing.Point(62, 186);
+            this.TextFromSpeech.Name = "TextFromSpeech";
+            this.TextFromSpeech.Size = new System.Drawing.Size(123, 26);
+            this.TextFromSpeech.TabIndex = 0;
+            this.TextFromSpeech.Text = "Listening ...";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(67, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(232, 221);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TextFromSpeech);
+            this.Name = "Form2";
+            this.Text = "SmartHome";
+            this.Shown += new System.EventHandler(this.Form2_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TextFromSpeech;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ Simple-Face-Recognition-App-CS-master/Simple Face Recognition App/Form2.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Speech.Recognition;
+using System.Net.Sockets;
+using System.IO;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace Simple_Face_Recognition_App
+{
+    public partial class Form2 : Form
+    {
+        string h;
+        static string ip;
+        static bool curtainsB, mistakeCurtains;
+        static System.Media.SoundPlayer player;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        static Label l;
+        static bool Mistake;
+        static void Connect(string Mes)
+        {
+            try
+            {
+                TcpClient tc = new TcpClient(ip, 80);
+                ASCIIEncoding asen = new ASCIIEncoding();
+                NetworkStream ns = tc.GetStream();
+                StreamWriter sw = new StreamWriter(ns);
+                byte[] ba = asen.GetBytes(Mes);
+                ns.Write(ba, 0, ba.Length);
+                mistakeCurtains = true;
+            }
+            catch
+            {
+                MessageBox.Show("Server connection error!");
+                Mistake = true;
+                mistakeCurtains = false;
+            }
+            if (!Mistake)
+            {
+                MessageBox.Show("Connected to  " + ip + " !");
+                Mistake = true;
+            }
+        }
+        static void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string words = "";
+            if (e.Result.Confidence > 0.5) { l.Text = e.Result.Text; words = e.Result.Text; }
+
+            if (words == "Google request")
+            {
+                Process.Start("https://www.google.com/search?q=" + "The weather in Warsaw");
+            }
+            if (words == "Open Youtube") { Process.Start("https://www.youtube.com/"); }
+
+            if (words == "Open Google") { Process.Start("http://google.com"); }
+
+            if (words == "turn on the light") { Connect("/LED=OFF1/r"); }
+            if (words == "turn off the light") { Connect("/LED=ON1/r"); }
+
+            if (words == "turn on the lamp") { Connect("/LED=OFF2/r"); }
+            if (words == "turn off the lamp") { Connect("/LED=ON2/r"); }
+
+            if (words == "turn on the outlet") { Connect("/LED=OFF3/r"); }
+            if (words == "turn off the outlet") { Connect("/LED=ON3/r"); }
+
+            if (words == "turn on the iron") { Connect("/LED=OFF5/r"); }
+            if (words == "turn off the iron") { Connect("/LED=ON5/r"); }
+
+            if (words == "turn on everything") { Connect("/LED=OFF4/r"); }
+            if (words == "turn off everything") { Connect("/LED=ON4/r"); }
+
+            if (words == "open curtains")
+            {
+                if (curtainsB == false)
+                {
+                    Connect("/LED=OFF6/r");
+                    if (mistakeCurtains == true)
+                    {
+                        curtainsB = true;
+                    }
+                    if (mistakeCurtains == false)
+                    {
+                        curtainsB = false;
+                    }
+                }
+            }
+            if (words == "close curtains")
+            {
+                if (curtainsB == true)
+                {
+                    Connect("/LED=ON6/r");
+                    if (mistakeCurtains == true)
+                    {
+                        curtainsB = false;
+                    }
+                    if (mistakeCurtains == false)
+                    {
+                        curtainsB = true;
+                    }
+                }
+            }
+        }
+
+        private void Form2_Shown(object sender, EventArgs e)
+        {
+            l = TextFromSpeech;
+
+            System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("en-us");// ru-ru
+            SpeechRecognitionEngine sre = new SpeechRecognitionEngine(ci);
+            sre.SetInputToDefaultAudioDevice();
+
+            sre.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sre_SpeechRecognized);
+
+
+            Choices numbers = new Choices();
+            numbers.Add(new string[] {"Open Google", "Open Youtube", "turn on the light", "turn off the light",
+                "open curtains", "close curtains","turn on the lamp","turn off the lamp","turn on the outlet",
+                "turn off the outlet","turn on the iron","turn off the iron","turn on everything",
+                "turn off everything","Google request" });
+
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Culture = ci;
+            gb.Append(numbers);
+
+
+            Grammar g = new Grammar(gb);
+            sre.LoadGrammar(g);
+
+            sre.RecognizeAsync(RecognizeMode.Multiple);//Single
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ip = textBox2.Text;
+            Mistake = false;
+            Connect(" ");
+        }
+    }
+}
